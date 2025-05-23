2c05734956847cab726b36610cc269bcef28c559
melmehdi <melmehdi@e1r8p5.1337.ma>
2025-05-23 11:50:38 +0100

    cpp 08

--- cpp08/ex01/Span.hpp
+++ cpp08/ex01/Span.hpp
@@ -5,7 +5,7 @@
 #include <vector>
 #include <algorithm>
 #include <stdexcept>
-
+#include <iostream>
 class Span {
 private:
     unsigned int _maxSize;
--- cpp08/ex01/main.cpp
+++ cpp08/ex01/main.cpp
@@ -1,24 +1,15 @@
 
 #include "Span.hpp"
-#include <iostream>
 
-int main() {
+int main()
+{
     Span sp = Span(5);
-
     sp.addNumber(6);
     sp.addNumber(3);
     sp.addNumber(17);
     sp.addNumber(9);
     sp.addNumber(11);
-
-    std::cout << "Shortest span: " << sp.shortestSpan() << std::endl;
-    std::cout << "Longest span: " << sp.longestSpan() << std::endl;
-
-    try {
-        sp.addNumber(42);
-    } catch (const std::exception &e) {
-        std::cerr << e.what() << std::endl;
-    }
-
+    std::cout << sp.shortestSpan() << std::endl;
+    std::cout << sp.longestSpan() << std::endl;
     return 0;
 }
